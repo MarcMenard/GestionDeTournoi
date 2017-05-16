1bf5f6ae8d9747887b06c1d068ecbec31791765e
hubertpineau <hubertpineau@hotmail.fr>
2017-05-16 22:20:10 +0200

    Application Locale
    Bouton début math et nom équipe relié

--- application_arbitre/app_arbitre/app/src/main/java/com/example/hubert/app_arbitre/Planning.java
+++ application_arbitre/app_arbitre/app/src/main/java/com/example/hubert/app_arbitre/Planning.java
@@ -20,9 +20,15 @@ import static com.example.hubert.app_arbitre.R.id.textView;
 
 public class Planning extends AppCompatActivity
 {
+
+  //  String textTeam[] = new [11];
+    private static String textTeam[] = {"a", "b", "c", "d", "e", "f", "g","h","i","j","k","l"};
+//    new textTeam[0] = "PNJ";
+
     //INITIALISE LE NOM DES DEUX EQUIPES
+
     private static String textTeamA = "Team X";
-    private static String textTeamB =  "Team Y";
+    private static String textTeamB = "Team Y";
 
 
     //GET PERMETTANT AU AUTRES ACTIVITE DE REPRENDRE SES DONNEES (pour les noms des equipes)
@@ -38,8 +44,28 @@ public class Planning extends AppCompatActivity
         setContentView(R.layout.activity_planning);
 
         //INITIALISE LES CHANGEMENTS DU NOM DES DEUX éQUIPES(VIS A VIS DU DéFAUT DU XML)
-            displayTextTeamB(textTeamB);
-            displayTextTeamA(textTeamA);
+
+        for (int h = 0; h < 12; h++)
+        {
+            displayTextTeamC(textTeam[h]);
+        }
+
+       // displayTextTeamB(textTeamA);
+       // displayTextTeamB(textTeamB);
+
+       /* android.view.View texxtTeam[]= new android.view.View[11];
+        texxtTeam[0] =  findViewById(R.id.textView0);
+        texxtTeam[1] =  findViewById(R.id.textView1);
+        texxtTeam[2] =  findViewById(R.id.textView2);
+        texxtTeam[3] =  findViewById(R.id.textView3);
+        texxtTeam[4] =  findViewById(R.id.textView4);
+        texxtTeam[5] =  findViewById(R.id.textView5);
+        texxtTeam[6] =  findViewById(R.id.textView6);
+        texxtTeam[7] =  findViewById(R.id.textView7);
+        texxtTeam[8] =  findViewById(R.id.textView8);
+        texxtTeam[9] =  findViewById(R.id.textView9);
+        texxtTeam[10] = findViewById(R.id.textView10);
+        texxtTeam[11] = findViewById(R.id.textView11);*/
 
         //TABLEAU POUR LES BOUTONS
         Button[] tab_boutons = new Button[6];
@@ -57,33 +83,200 @@ public class Planning extends AppCompatActivity
         }
 
         //ACTIONS DE CHACUN DES BOUTONS --> AMENER AU MAINACTIVITY
+
         for(int x = 0; x < 6; x++)
         {
             tab_boutons[x].setOnClickListener(new View.OnClickListener()
             {
                 @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(view.getContext(), MainActivity.class);
-                    Button button = (Button) view;
+                public void onClick(View view)
+                {
                     startActivity(new Intent(getApplicationContext(), MainActivity.class));
                 }
             });
+
+
+            tab_boutons[0].setOnClickListener(new View.OnClickListener()
+                {
+
+                    @Override
+                    public void onClick(View view)
+                    {
+                        textTeamA = textTeam[0];
+                        textTeamB = textTeam[1];
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                    }
+                });
+
+            tab_boutons[1].setOnClickListener(new View.OnClickListener()
+            {
+
+                @Override
+                public void onClick(View view)
+                {
+                    textTeamA = textTeam[2];
+                    textTeamB = textTeam[3];
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+            });
+
+            tab_boutons[2].setOnClickListener(new View.OnClickListener()
+            {
+
+                @Override
+                public void onClick(View view)
+                {
+                    textTeamA = textTeam[4];
+                    textTeamB = textTeam[5];
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+            });
+
+            tab_boutons[3].setOnClickListener(new View.OnClickListener()
+            {
+
+                @Override
+                public void onClick(View view)
+                {
+                    textTeamA = textTeam[6];
+                    textTeamB = textTeam[7];
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+            });
+
+            tab_boutons[4].setOnClickListener(new View.OnClickListener()
+            {
+
+                @Override
+                public void onClick(View view)
+                {
+                    textTeamA = textTeam[8];
+                    textTeamB = textTeam[9];
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+            });
+
+            tab_boutons[5].setOnClickListener(new View.OnClickListener()
+            {
+
+                @Override
+                public void onClick(View view)
+                {
+                    textTeamA = textTeam[10];
+                    textTeamB = textTeam[11];
+                    startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                }
+            });
+
+
+
+        }}
+
+
+
+    private void displayTextTeamC(String text)
+    {
+        /*android.view.View texxtTeam[]= new android.view.View[11];
+        texxtTeam[0] =  findViewById(R.id.textView0);
+        texxtTeam[1] =  findViewById(R.id.textView1);
+        texxtTeam[2] =  findViewById(R.id.textView2);
+        texxtTeam[3] =  findViewById(R.id.textView3);
+        texxtTeam[4] =  findViewById(R.id.textView4);
+        texxtTeam[5] =  findViewById(R.id.textView5);
+        texxtTeam[6] =  findViewById(R.id.textView6);
+        texxtTeam[7] =  findViewById(R.id.textView7);
+        texxtTeam[8] =  findViewById(R.id.textView8);
+        texxtTeam[9] =  findViewById(R.id.textView9);
+        texxtTeam[10] = findViewById(R.id.textView10);
+        texxtTeam[11] = findViewById(R.id.textView11);*/
+
+        switch (text)
+        {
+            case "a":
+            TextView TextView0 = (TextView) findViewById(R.id.textView0);
+            TextView0.setText(String.valueOf(text));
+                break;
+
+            case "b":
+            TextView TextView1 = (TextView) findViewById(R.id.textView1);
+            TextView1.setText(String.valueOf(text));
+                break;
+
+            case "c":
+            TextView TextView2 = (TextView) findViewById(R.id.textView2);
+            TextView2.setText(String.valueOf(text));
+                break;
+
+            case "d":
+            TextView TextView3 = (TextView) findViewById(R.id.textView3);
+            TextView3.setText(String.valueOf(text));
+                break;
+
+            case "e":
+            TextView TextView4 = (TextView) findViewById(R.id.textView4);
+            TextView4.setText(String.valueOf(text));
+                break;
+
+            case "f":
+            TextView TextView5 = (TextView) findViewById(R.id.textView5);
+            TextView5.setText(String.valueOf(text));
+                break;
+
+            case "g":
+            TextView TextView6 = (TextView) findViewById(R.id.textView6);
+            TextView6.setText(String.valueOf(text));
+                break;
+
+            case "h":
+            TextView TextView7 = (TextView) findViewById(R.id.textView7);
+            TextView7.setText(String.valueOf(text));
+                break;
+
+            case "i":
+            TextView TextView8 = (TextView) findViewById(R.id.textView8);
+            TextView8.setText(String.valueOf(text));
+                break;
+
+            case "j":
+            TextView TextView9 = (TextView) findViewById(R.id.textView9);
+            TextView9.setText(String.valueOf(text));
+                break;
+
+            case "k":
+            TextView TextView10 = (TextView) findViewById(R.id.textView10);
+            TextView10.setText(String.valueOf(text));
+                break;
+
+            case "l":
+            TextView TextView11 = (TextView) findViewById(R.id.textView11);
+            TextView11.setText(String.valueOf(text));
+                break;
         }
     }
+
+
+
+   /* private void displayTextTeamC(String text)
+    {
+        TextView TextView = (TextView) texxtTeam[cpt];
+        TextView.setText(String.valueOf(text));
+    }*/
+
 
 
 
     //VA CHANGER LE NOM DE l'EQUIPE DE L'EQUIPE du TEXTE SELECTIONNE
     private void displayTextTeamA(String text)
     {
-        TextView TextView = (TextView) findViewById(R.id.textView13);
+        TextView TextView = (TextView) findViewById(R.id.textView8);
         TextView.setText(String.valueOf(text));
     }
+
 
     //VA CHANGER LE NOM DE l'EQUIPE DE L'EQUIPE du TEXTE SELECTIONNE
     private void displayTextTeamB(String text)
     {
-        TextView TextView = (TextView) findViewById(R.id.textView14);
+        TextView TextView = (TextView) findViewById(R.id.textView9);
         TextView.setText(String.valueOf(text));
     }
 
@@ -107,3 +300,8 @@ public class Planning extends AppCompatActivity
                 .show();
     }
 }
+
+
+//3 variable EXEMPLE : H, U ET B --> U = 2*H  ET B = (2*H)+1
+
+//if tab_boutons[0] else textView0 et text
